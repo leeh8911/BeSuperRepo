b0b61a47e5d8d073ee8e6e8c709c099d06e64f75
sangwon lee <54252269+leeh8911@users.noreply.github.com>
2021-08-04 06:39:53 +0900

    svelte-and-sapper-in-action study add
    Former-commit-id: 643af1b4f3434390088f4d14d89e9a4b3ebe101e

---
+++ Research/svelte-sapper-in-action/ch2/hello app/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Clock from './Clock.svelte'
+	let color = "red";
+	let name = 'world';
+	let upper = false;
+	
+	$: greeting = `Hello ${name}!`;
+</script>
+
+<label for="name">Name</label>
+<input id="name" bind:value={name}>
+
+<label for="color">Color</label>
+<input id="color" type="color" bind:value={color}>
+<div style="background-color: {color}" class="swatch"/>
+
+<h1 style="color: {color}">{greeting}</h1>
+
+<style>
+	.swatch {
+		display: inline-block;
+		height: 20px;
+		width: 20px;
+	}
+</style>
+<Clock {color}/>
---
+++ Research/svelte-sapper-in-action/ch2/hello app/src/Clock.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let color = 'blue';
+	let hhmmss = '';
+	setInterval(() => {
+		hhmmss = new Date().toLocaleTimeString();
+	}, 1000);
+</script>
+
+<span style="color: {color}">{hhmmss}</span>
