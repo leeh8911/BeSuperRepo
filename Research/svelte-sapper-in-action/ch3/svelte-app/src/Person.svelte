2b52695f47fa3852ecd337cdbc18f9d178304970
sangwon lee <54252269+leeh8911@users.noreply.github.com>
2021-08-08 18:23:32 +0900

    update travel-checklist
    Former-commit-id: 07cf7e089983e6df27f4e23c0c7ab36d404668a8

---
+++ Research/svelte-sapper-in-action/ch3/svelte-app/src/Person.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let age;
+    export let ball;
+    export let developer;
+    export let favoriteColors;
+    export let fullName;
+    export let onBirthdat;
+</script>
+
+<div>
+    {fullName} is {age} years old and {developer ? 'is' : 'is not'} a developer.
+</div>
+<div>
+    They like the colors {favoriteColors.join(' and ')}.
+</div>
+<div>
+    They like to throw {ball.new ? "a new" : "an old"} {ball.name} that weights {ball.grams} grams.
+</div>
+<button on:click={onBirthday}>It's my birthday!</button>
+
+
+
+<style>
+
+
+</style>
